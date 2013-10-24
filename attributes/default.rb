5d803820e3a0ef120e8a87f06d9606eb7b0d1216
Benjamin Abbott-Scott <babbottscott@zendesk.com>
2013-10-24 11:48:28 -0700

    make travis happy

--- attributes/default.rb
+++ attributes/default.rb
@@ -95,7 +95,7 @@ default['datadog']['syslog']['port'] = nil
 # in this cookbook, and apply them to the appropirate node's run list.
 # Read more at http://docs.datadoghq.com/
 
-# For older integrations that do not consume the conf.d yaml files
+# For older integrations that do not consume the conf.d yaml files 
 default['datadog']['legacy_integrations']['nagios']['enabled'] = false
 default['datadog']['legacy_integrations']['nagios']['description'] = "Nagios integration"
 default['datadog']['legacy_integrations']['nagios']['config']['nagios_log'] = '/var/log/nagios3/nagios.log'
